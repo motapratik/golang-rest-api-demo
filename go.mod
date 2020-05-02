824fc39245b7683e95efba691157f63f9e896431
Pratik <mota.pratik@gmail.com>
2020-05-02 19:18:34 +0530

    introducing mod  and gorilamux

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/motapratik/golang-rest-api-demo
+
+go 1.14
+
+require github.com/gorilla/mux v1.7.4
--- main.go
+++ main.go
@@ -1,28 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprintf(w, "hello\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
+func test(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("This is Testing API"))
 }
 
 func main() {
 
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
+	router := mux.NewRouter()
+	router.HandleFunc("/test", test)
 
-	http.ListenAndServe(":8090", nil)
+	http.ListenAndServe(":8090", router)
 }
