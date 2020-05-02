77373f4e6e4320ae98c4597a8c3a2f560232f947
Pratik <mota.pratik@gmail.com>
2020-05-02 19:50:32 +0530

    json encoder adding

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func test(w http.ResponseWriter, req *http.Request) {
+func test1(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("This is Testing API"))
+}
+
+func test2(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(struct {
+		ID string
+	}{"123"})
 }
 
 func main() {
 
 	router := mux.NewRouter()
-	router.HandleFunc("/test", test)
-
+	router.HandleFunc("/test1", test1)
+	router.HandleFunc("/test2", test2)
 	http.ListenAndServe(":8090", router)
 }
